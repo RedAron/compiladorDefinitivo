acae6e273e01184a8747c47b3a1c83cc40e124f1
Janer Gonzalez <janer369@gmail.com>
2018-05-18 19:41:28 -0500

    Creacion de la aplicacion y modelo para usuario

---
+++ proyecto/apps/usuario/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+
+class Usuario(AbstractUser):
+	edad = models.CharField(max_length=50)
+	ciudad = models.CharField(max_length=50)
+	pais = models.CharField(max_length=50)
+	lenguaje = models.CharField(max_length=50)
+
+	
--- proyecto/compilador/settings.py
+++ proyecto/compilador/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'apps.usuario',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTH_USER_MODEL = 'usuario.Usuario'
