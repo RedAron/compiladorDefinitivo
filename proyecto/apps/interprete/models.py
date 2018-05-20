497afa160335b00ec498bdc8484ef02a835ed1eb
janer gonzalez <janer369@gmail.com>
2018-05-20 12:48:39 -0500

    arreglando cambios en el interprete

--- proyecto/apps/interprete/models.py
+++ proyecto/apps/interprete/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+import datetime
+
+
+# Create your models here.
+
+class Programa(models.Model):
+    nombre=models.CharField(max_length=30)
+    script=models.TextField()
+    tiempo=models.DateField(("Date"),default=datetime.date.today)
+    
