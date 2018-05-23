3e1e86fd50c5a289610b3b46c3196418fb30f987
janer gonzalez <janer369gmail.com>
2018-05-23 13:20:40 -0500

    creada larelacion

--- proyecto/apps/interprete/admin.py
+++ proyecto/apps/interprete/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Programa
+
+@admin.register(Programa)
+class adminPrograma(admin.ModelAdmin):
+    list_display=('nombre','script','tiempo')
+
--- proyecto/apps/interprete/models.py
+++ proyecto/apps/interprete/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 
 # Create your models here.
-
+from apps.usuario.models import Usuario
 import datetime
 
 
 # Create your models here.
 
 class Programa(models.Model):
+   
     nombre=models.CharField(max_length=30)
     script=models.TextField()
     tiempo=models.DateField(("Date"),default=datetime.date.today)
+    usuario=models.ForeignKey(Usuario,on_delete=models.CASCADE)
     
