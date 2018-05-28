eb83c00ea6d18c98afbab2d807737e67a1b7dc25
janer gonzalez <janer369gmail.com>
2018-05-28 13:46:58 -0500

    ya funcina el crud

--- proyecto/apps/usuario/urls.py
+++ proyecto/apps/usuario/urls.py
@@ -3,8 +3,13 @@ from django.conf.urls import url
 from apps.usuario.views import *
 from . import views
 
+
 urlpatterns = [
 	url(r'^$', welcome, name="welcome"),
+    url(r'^home/diagrama/$', diagramas, name="diagrama"),
     url(r'^registro', RegistroUsuario.as_view(), name="registro"),
+    
+    url(r'^usuario/editar/(?P<pk>\d+)$', actualizarUsuario.as_view(), name='editar'),
+    url(r'^usuario/borrar/(?P<pk>\d+)$', eliminarUsuario.as_view(), name='eliminar'),
 
 ]
--- proyecto/apps/usuario/views.py
+++ proyecto/apps/usuario/views.py
@@ -5,10 +5,14 @@ from django.template import loader
 from apps.usuario.models import Usuario
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
+
+
 from django.core.urlresolvers import reverse_lazy
 
 from apps.usuario.forms import UsuarioForm
-
+import json
 def welcome(request):
     template=loader.get_template('usuario/welcome.html')
     context={
@@ -16,10 +20,38 @@ def welcome(request):
     }
     return HttpResponse(template.render(context,request))
 
+def diagramas(request):
+    queryset=Usuario.objects.all()
+    name=[obj.username for obj in queryset]
+    context={
+        'name':json.dumps(name)
+    }
+    return render(request,'usuario/estadisticas.html',context)
+
+
+
+
 class RegistroUsuario(CreateView):
     model = Usuario
     template_name = 'usuario/registro.html'
     form_class = UsuarioForm
-    success_url = reverse_lazy('usuario:welcome')
+    success_url = reverse_lazy('login')
+
+class actualizarUsuario(UpdateView):
+    model=Usuario
+    success_url=reverse_lazy('login')
+    fields=['username',
+                'first_name',
+                'last_name',
+                'email',
+                'edad',
+                'ciudad',
+                'pais',
+                'lenguaje',
+            ]
+class eliminarUsuario(DeleteView):
+    template_name='usuario/eliminar.html'
+    model=Usuario
+    success_url=reverse_lazy('logout')
 
 
